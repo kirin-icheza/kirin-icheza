1c59b86482d3e5d946e92c7aee705552ede12356
Hyenakim <gpsk1202@naver.com>
2019-12-06 22:23:59 +0900

    2019-12-06 update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, Response
-from video_pose import VideoCamera
 import time
 import picamera
+import cv2
+from pose_detector import *
 
 app = Flask(__name__)
 
@@ -21,14 +22,24 @@ def add_header(response):
 def show_video():
     return render_template("main.html")
 
-def gen(camera):
+def gen(overlay_pose=False):
+    interpreter = load_model()
+    cam = cv2.VideoCapture(0)
     while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        ret_val, img = cam.read()
+        if overlay_pose:
+            img = annotate_img(img, interpreter)
+
+        temp, jpeg = cv2.imencode('.jpg', img)
+        img = jpeg.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n\r\n')
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')        
+    return Response(gen(overlay_pose=True), mimetype='multipart/x-mixed-replace; boundary=frame')
         
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port =80, debug=True)
