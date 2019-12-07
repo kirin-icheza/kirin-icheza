0f875f14614f13d8e58fe44c43f0fbc7e69fcb82
Hyenakim <gpsk1202@naver.com>
2019-12-07 21:38:37 +0900

    update pose_detector.py

--- pose_detector.py
+++ pose_detector.py
@@ -3,6 +3,36 @@ import numpy as np
 import cv2
 from person import BodyPart, KeyPoint, Position, Person
 
+
+# img data 표준화 -1 ~ 1
+def standardize_image(img):
+    mean = 128.0
+    std = 128.0
+    img_norm = (img - mean) / std
+    img_norm = img_norm.astype("float32")
+    return img_norm
+
+def resize_image(img, dsize=(257,257)):
+    # INTER_AREA interpolation is faster than INTER_CUBIC
+    # print('image shape before resize_image ', img.shape) # (480, 640, 3)
+    reshaped_img = cv2.resize(img, dsize=dsize, interpolation=cv2.INTER_AREA)
+    # print('image shape in resize_image ', reshaped_img.shape) # (257, 257, 3)
+    reshaped_img = reshaped_img.reshape(1, 257, 257, 3)
+    return reshaped_img
+
+def scale_image(img):
+    height, width, _ = img.shape # (480, 640, 3)
+    height_scale = height / 257
+    width_scale = width / 257
+    # print('scale_image height ,width : ', img.shape )
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = resize_image(img) # img = (1, 257, 257, 3)
+    img = standardize_image(img) # -1 < img < 1
+    return img, height_scale, width_scale
+
+
+
 def annotate_img(img, interpreter):
     scaled_image, height_scale, width_scale = scale_image(img) # img scaling
     person = detect_person(scaled_image, interpreter)
